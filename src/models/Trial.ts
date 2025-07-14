1694ee04f2fe226d34a5b6cf0d0781fd491beb6e
VICKY SHARMA <lucky170313@gmail.com>
2025-07-14 08:04:15 +0000

    fetch properly

--- src/models/Trial.ts
+++ src/models/Trial.ts
@@ -9,7 +9,7 @@ const TrialSchema = new Schema({
   purifierName: { type: String },
   planName: { type: String },
   tenure: { type: String },
-  createdAt: { type: Date, default: Date.now, required: true },
+  createdAt: { type: Date, default: Date.now },
   status: { type: String, enum: ['New', 'Contacted', 'Resolved', 'Closed'], default: 'New' },
   type: { type: String, default: 'trial' },
 });
