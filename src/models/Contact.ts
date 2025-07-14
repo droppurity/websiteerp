9a271d52300deff9606cd9fd2dc88d0cc74157f1
VICKY SHARMA <lucky170313@gmail.com>
2025-07-14 09:18:16 +0000

    not fixed

--- src/models/Contact.ts
+++ src/models/Contact.ts
@@ -3,9 +3,9 @@ import mongoose, { Schema, models } from 'mongoose';
 const ContactSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  createdAt: { type: Date, required: true, default: Date.now },
-  status: { type: String, enum: ['New', 'Contacted', 'Resolved', 'Closed'], default: 'New' },
   message: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  status: { type: String, enum: ['New', 'Contacted', 'Resolved', 'Closed'], default: 'New' },
   type: { type: String, default: 'contact' },
 }, {
   collection: 'contacts' // Explicitly set the collection name
--- src/models/Referral.ts
+++ src/models/Referral.ts
@@ -3,8 +3,8 @@ import mongoose, { Schema, models } from 'mongoose';
 const ReferralSchema = new Schema({
   customerId: { type: String },
   friendName: { type: String, required: true },
-  friendAddress: { type: String },
   friendMobile: { type: String },
+  friendAddress: { type: String },
   createdAt: { type: Date, default: Date.now },
   status: { type: String, enum: ['New', 'Contacted', 'Resolved', 'Closed'], default: 'New' },
   type: { type: String, default: 'referral' },
